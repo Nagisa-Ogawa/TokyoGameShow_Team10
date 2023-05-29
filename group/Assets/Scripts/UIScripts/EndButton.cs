5595670e6e32463573d5cdd4347b25c0a4e09e70
satoharuki122 <hal.sato.haruki@gmail.com>
2023-05-29 12:26:20 +0900

    セレクト画面の追加

---
+++ group/Assets/Scripts/UIScripts/EndButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class EndButton : MonoBehaviour
+{
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+      Application.Quit();
+#endif
+    }
+}
---
+++ group/Assets/Scripts/UIScripts/Yoko_SelectButton.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Yoko_SelectButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button[] buttons;
+    private int selectedIndex = 0;
+    bool once = false;
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        bool submitInput = Input.GetButtonDown("Submit");
+
+        if (!once)
+        {
+            // ボタンの選択
+            if (horizontalInput < 0)
+            {
+                selectedIndex--;
+                if (selectedIndex < 0)
+                    selectedIndex = buttons.Length - 1;
+                once = true;
+            }
+            else if (horizontalInput > 0)
+            {
+                selectedIndex++;
+                if (selectedIndex >= buttons.Length)
+                    selectedIndex = 0;
+                once = true;
+            }
+        }
+
+        if (horizontalInput == 0)
+            once = false;
+
+        // ボタンの選択状態を更新
+        for (int i = 0; i < buttons.Length; i++)
+        {
+            if (i == selectedIndex)
+                buttons[i].Select();
+            else
+                buttons[i].OnDeselect(null);
+        }
+
+        // Aボタンで選択したボタンの処理を実行
+        if (submitInput)
+            buttons[selectedIndex].onClick.Invoke();
+    }
+}
